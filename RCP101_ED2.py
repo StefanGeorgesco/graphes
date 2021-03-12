04d424cdec7a9891e944724ad33f466b82d96a68
Stefan Georgesco <stefan.georgesco@yahoo.fr>
2021-03-12 18:35:17 +0100

    Add ED2 exercises 3 & 4

--- RCP101_ED2.py
+++ RCP101_ED2.py
@@ -14,12 +14,17 @@ def chemin(pere, s1, s2):
         return []
     return chemin(pere, s1, pere[s2]) + [(pere[s2], s2)]
 
+def chemin2(P, s1, s2):
+    if s1 == s2:
+        return []
+    return chemin2(P, s1, P[s1, s2]) + [(P[s1,s2], s2)]
+
 graphe, sommets = get_graphes_orientes("RCP101 - ED2 - Exercice 1")
 print(graphe.getCommentaire())
 T1A0 = sommets[0]
 T6 = sommets[15]
 pi, pere = graphe.bellman(T1A0)
-print(f"plus court chemin de {T1A0} à {T6}", chemin(pere, T1A0, T6))
+print(f"plus court chemin de {T1A0} à {T6} :", chemin(pere, T1A0, T6))
 print("pi :", pi)
 print("père :", pere)
 print()
@@ -31,3 +36,59 @@ pi, pere = graphe.dijkstra(x1)
 print("pi :", pi)
 print("père :", pere)
 print()
+
+graphe, sommets = get_graphes_orientes("RCP101 - ED2 - Exercice 3 questions 1 et 2")
+print(graphe.getCommentaire())
+S1 = sommets[0]
+S10 = sommets[9]
+pi, pere = graphe.bellman(S1)
+print(f"plus court chemin de {S1} à {S10} :", chemin(pere, S1, S10))
+print(f"distance de {S1} à {S10} :", pi[S10])
+print("pi :", pi)
+print("père :", pere)
+print()
+
+graphe, sommets = get_graphes_orientes("RCP101 - ED2 - Exercice 3 question 3")
+print(graphe.getCommentaire())
+S1 = sommets[0]
+S10 = sommets[9]
+pi, pere = graphe.bellman(S1)
+print(f"plus court chemin de {S1} à {S10} :", chemin(pere, S1, S10))
+print(f"distance de {S1} à {S10} :", pi[S10])
+print("pi :", pi)
+print("père :", pere)
+print()
+
+graphe, sommets = get_graphes_orientes("RCP101 - ED2 - Exercice 4")
+print(graphe.getCommentaire())
+try:
+    M, P = graphe.floyd_warshall()
+except Exception as e:
+    print("\tIl y a un circuit absorbant. Fin de l'algorithme de Floyd-Warshall : " + repr(e))
+print("\tmatrice M de Floyd-Warshall :")
+print("\t" + "".ljust(5), end="")
+for j in sommets:
+    print(repr(j).ljust(5), end="")
+print()
+for i in sommets:
+    print("\t" + repr(i).ljust(5), end="")
+    for j in sommets:
+        print(repr(M[i, j]).ljust(5), end="")
+    print()
+print()
+print("\tmatrice P de Floyd-Warshall :")
+print("\t" + "".ljust(5), end="")
+for j in sommets:
+    print(repr(j).ljust(5), end="")
+print()
+for i in sommets:
+    print("\t" + repr(i).ljust(5), end="")
+    for j in sommets:
+        print(repr(P[i, j]).ljust(5), end="")
+    print()
+print()
+for s1, s2 in [(x,y) for x in sommets for y in sommets if x != y]:
+    if M[s1, s2] == float('inf'):
+        print(f"Il n'y a pas de plus court chemin de {s1} à {s2}.")
+    else:
+        print(f"Plus court chemin de {s1} à {s2} :", chemin2(P, s1, s2), "- distance :", M[s1, s2])
