65bf7507301d19ed7b1da396b39b0a425e1ae738
Stefan Georgesco <stefan.georgesco@yahoo.fr>
2021-03-25 20:59:33 +0100

    MPM graph new example + display margins in calculs_graphes_MPM.py

--- calculs_graphes_MPM.py
+++ calculs_graphes_MPM.py
@@ -1,7 +1,9 @@
-from graphesMPM_exemples import graphes_MPM
+from graphesMPM_exemples import graphes_MPM, Tache
 
 for nom in graphes_MPM.keys():
     graphe_MPM = graphes_MPM[nom]
     print(graphe_MPM)
     print(f"\tDate de fin : {graphe_MPM.date_de_fin()}\n")
     print(f"\tTâches critiques : {graphe_MPM.taches_critiques()}\n\n")
+    for tache in sorted(graphe_MPM.getTaches(), key=Tache.__repr__):
+        print(f"\tTâche {tache}, marge libre : {tache.getMarge_libre()}, marge totale : {tache.marge_totale()}")
